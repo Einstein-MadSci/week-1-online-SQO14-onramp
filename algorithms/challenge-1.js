522cd26c8c773a4191d50c94be449cbbeff265cd
Einstein-MadSci <mrakinyanmi@gmail.com>
2022-10-28 16:16:59 +0100

    completed truthy values program

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,11 @@
 function countTruthy(arr) {
-
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      count += 1;
+    }
+  }
+  return count;
 }
-module.exports = countTruthy
+
+module.exports = countTruthy;

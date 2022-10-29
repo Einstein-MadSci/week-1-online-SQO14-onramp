cbd5d3c4cbc5d63f7a37f2fa4d28a06f56819516
Einstein-MadSci <mrakinyanmi@gmail.com>
2022-10-29 11:26:46 +0100

    Completed challenge 4

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,32 @@
 function calculateGrade(marks) {
- 
- }
-  module.exports =calculateGrade
+  let numberOfGrades = marks.length;
+  let totalOfGrades = 0;
+  let gradesAverage = 0;
+  for (let i = 0; i < marks.length; i++) {
+    totalOfGrades += marks[i];
+  }
+  gradesAverage = Math.floor(totalOfGrades / numberOfGrades);
+
+  if (gradesAverage >= 1 && gradesAverage <= 49) {
+    // console.log("F");
+    return "F";
+  } else if (gradesAverage >= 50 && gradesAverage <= 59) {
+    // console.log("E");
+    return "E";
+  } else if (gradesAverage >= 60 && gradesAverage <= 69) {
+    // console.log("D");
+    return "D";
+  } else if (gradesAverage >= 70 && gradesAverage <= 79) {
+    // console.log("C");
+    return "C";
+  } else if (gradesAverage >= 80 && gradesAverage <= 89) {
+    // console.log("B");
+    return "B";
+  } //(gradesAverage >= 90 && gradesAverage <= 100)
+  else {
+    // console.log(gradesAverage, "A");
+    return "A";
+  }
+}
+
+module.exports = calculateGrade;
